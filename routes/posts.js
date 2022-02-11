8b4ba0b095ca2b75151b854defc4720dd224b5a8
Ferrois <ferrois2005@gmail.com>
2022-02-11 21:57:32 +0800

    commit

--- routes/posts.js
+++ routes/posts.js
@@ -20,8 +20,9 @@ function authenticateToken(req, res, next) {
 }
 
 router.get("/", async (req, res) => {
+  console.log("123")
   const posts = await PostSchema.find().limit(20);
-  console.log(posts);
+  console.log("456");
   res.json(posts);
 });
 
