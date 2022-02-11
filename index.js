cf4ea0540d39e7304c565a232a32b9762e635da7
Ferrois <ferrois2005@gmail.com>
2022-02-11 22:20:52 +0800

    ni2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const ChatSchema = require("./models/Chat.js");
 // }
 // const jwt = require("jsonwebtoken");
 
-app.get("/teste", authenticateToken, async (req,res) => {
+app.get("/teste", async (req,res) => {
   const chats = await ChatSchema.find().limit(100);
   res.json(chats);
 })
